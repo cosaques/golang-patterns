6ed4891d967d349b63bc46f8b3fae62d068917ca
Artem MINIAILO <aminiailo@vente-privee.com>
2018-02-06 12:50:27 +0100

    make code more visible

--- observer/main.go
+++ observer/main.go
@@ -5,15 +5,9 @@ import observer "github.com/cosaques/patterns/observer/src"
 func main() {
 	weatherData := new(observer.WeatherData)
 	currentConditionsDisplay := new(observer.CurrentConditionsDisplay)
-	currentConditionsDisplay.Display()
-
 	weatherData.RegisterObserver(currentConditionsDisplay)
+
 	weatherData.SetMeasurements(32, 75, 500)
-
-	weatherData.RemoveObserver(currentConditionsDisplay)
 	weatherData.SetMeasurements(35, 74, 400)
-
-	weatherData.RegisterObserver(currentConditionsDisplay)
-	weatherData.RegisterObserver(currentConditionsDisplay)
 	weatherData.SetMeasurements(36, 73, 300)
 }
