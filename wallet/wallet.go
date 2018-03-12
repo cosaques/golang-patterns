5eed42cc2e06b6f5b2acad8ca6ca666be3ec336f
cosaques <tioma@outlook.com>
2018-03-12 10:56:21 +0100

    prettify code

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -5,22 +5,30 @@ type wallet struct {
 }
 
 func (w *wallet) GetValue(currency string, rate Rate) float64 {
-	result := 0.0
 	if rate == nil {
 		return w.amounts[currency]
 	}
-	for c, value := range w.amounts {
-		r := 1.0
-		if c != currency {
-			r = rate.GetRate(c, currency)
-		}
-		result += value * r
-	}
-	return result
+	return w.getTotalValue(currency, rate)
 }
 
 func (w *wallet) Add(currency string, amount float64) {
 	w.amounts[currency] += amount
+}
+
+func (w *wallet) getTotalValue(toCurrency string, rate Rate) float64 {
+	total := 0.0
+	for currency, value := range w.amounts {
+		total += value * computeRate(currency, toCurrency, rate)
+	}
+	return total
+}
+
+// ternaire
+func computeRate(fromCurrency string, toCurrency string, rate Rate) float64 {
+	if fromCurrency != toCurrency {
+		return rate.GetRate(fromCurrency, toCurrency)
+	}
+	return 1.0
 }
 
 func NewWallet() *wallet {
